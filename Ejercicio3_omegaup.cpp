a55e205919578ebd9e44e70a80f3654806f95741
cvilcap <cvilcap@unjbg.edu.pe>
2026-06-21 14:04:11 -0500

    Ejercicio finalizado

--- Ejercicio3_omegaup.cpp
+++ Ejercicio3_omegaup.cpp
@@ -3,4 +3,13 @@
     int main(){
         int P,n,v;
         int A[101]={0};
- 
+        cin>>P>>n;
+        for(int i=0;i<n;i++){
+            cin>>v;
+            A[v]++;
+        }
+        for(int i=1;i<=P;i++){
+            cout<<i<<"-"<<A[i]<<"\n";
+        }
+        return 0;
+    }
